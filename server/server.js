7060c7dc88ddb00e5316e8aca55ed4003e508942
cEbuka <kastecebuka@gmail.com>
2025-02-11 23:56:47 +0100

    added backend for api

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/movies", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+});
