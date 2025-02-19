d8131f6fa92415a421563f26d6351f5701b52bdf
cEbuka <kastecebuka@gmail.com>
2025-02-19 09:41:08 +0100

    fixing movie card

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -6,21 +6,19 @@ const MoviesList = () => {
     const { movies } = useMovieData()
     return (
         <Fragment>
-
-            <div className="movies-section">
-                <Hero />
-                <h1>Featured Movies</h1>
-                <div className="movie-card">
-                    {movies?.map((movie) =>
-                        <div key={movie.id}>
-                            <img className="movie-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie_picture" />
-                            <div className="movie-details">
-                                <span data-testid="movie-title" className="movie-title">{movie.title}</span>
-                                <span data-testid="movie_overview" >{movie.overview}</span>
-                            </div>
-                        </div>)}
-                </div>
+            <Hero />
+            <h1>Featured Movies</h1>
+            <div className="movie-list">
+                {movies?.map((movie) =>
+                    <div key={movie.id} className="movie-card">
+                        <img className="movie-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie_picture" />
+                        <div className="movie-details">
+                            <span className="movie-title">{movie.title}</span>
+                            <span className="movie-overview">{movie.overview}</span>
+                        </div>
+                    </div>)}
             </div>
+
         </Fragment>
     )
 }
