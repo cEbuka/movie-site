1795229549312d7a3fd9a44dd82f530a8dcb3f55
cEbuka <kastecebuka@gmail.com>
2025-03-09 23:37:17 +0100

    styled nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,8 @@ const Header = () => {
                 </Form>
 
                 <div className="sign-in">
-                    <Link to={`/signup`} className="sign_in">Sign in
+                    <Link to={`/signup`} className="sign_in">
+                        <h3>Sign in</h3>
                     </Link>
                     <button><FaBars /></button>
                 </div>
